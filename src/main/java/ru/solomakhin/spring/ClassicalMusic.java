edc0e74a09c065239cc75e9bb9de4dd64b995c70
Solomakhin Anton <s.anton45@yandex.ru>
2021-10-24 22:09:53 +0300

    Initial commit

---
+++ src/main/java/ru/solomakhin/spring/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.solomakhin.spring;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Classic";
+    }
+}
---
+++ src/main/java/ru/solomakhin/spring/Music.java
@@ -0,0 +1,5 @@
+package ru.solomakhin.spring;
+
+public interface Music {
+    String getSong();
+}
